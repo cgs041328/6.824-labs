c3adff555b2bfd3bbaeee4fe4f8bfc949df97f8b
cgs <cgs931219@outlook.com>
2018-02-01 14:08:56 +0800

    resolve channel block bug

--- src/mapreduce/master.go
+++ src/mapreduce/master.go
@@ -107,7 +107,7 @@ func Distributed(jobName string, files []string, nreduce int, master string) (mr
 	mr.startRPCServer()
 	go mr.run(jobName, files, nreduce,
 		func(phase jobPhase) {
-			ch := make(chan string)
+			ch := make(chan string,len(files))
 			go mr.forwardRegistrations(ch)
 			schedule(mr.jobName, mr.files, mr.nReduce, phase, ch)
 		},
